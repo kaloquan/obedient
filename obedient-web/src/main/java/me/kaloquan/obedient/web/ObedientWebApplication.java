d3a67694dd401f4125058be27cd87b3cdb1f12ef
Kalo Quan <KaloQuan@163.com>
2017-09-30 16:47:16 +0800

    Initial commit

---
+++ obedient-web/src/main/java/me/kaloquan/obedient/web/ObedientWebApplication.java
@@ -0,0 +1,12 @@
+package me.kaloquan.obedient.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObedientWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObedientWebApplication.class, args);
+	}
+}
